57df4d829de530f49a38e1ac7a1650fec5ee64c6
Ikemba-chibueze <chibueze@vastracktech.com.ng>
2025-11-21 14:20:54 +0100

    feat(VBE-6): added swagger type documentation

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
 import { AppController } from './app.controller';
+import { PaymentModule } from './payment/payment.module';
 
 @Module({
   imports: [
     DatabaseModule,
+    PaymentModule,
     ConfigModule.forRoot({ isGlobal: true }),
   ],
   controllers: [AppController],
